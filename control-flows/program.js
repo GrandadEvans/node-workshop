8d136d695375c523d9ab8aab8850e2b34726caa9
Stephen Melrose <me@stephenmelrose.co.uk>
2014-10-16 20:04:50 +0100

    Added URLs for control flow exercise

---
+++ control-flows/program.js
@@ -0,0 +1,9 @@
+
+var urls = [
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=1',
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=2',
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=3',
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=4',
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=5',
+    'http://node-workshop.stephenmelrose.co.uk/number.php?number=6'
+];
